e9c644ecf1fbdcfc03e0c8748dd01e220092addc
WangJiaxiang96 <melody_mylady@outlook.com>
2016-12-27 01:08:50 +0800

    Update

---
+++ js/nav.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+function widthIncreased(){
+	"use strict";
+	var v=document.getElementById('div_tittle');
+	v.style.cssText="width:950px;";
+}
+function widthDecreased(){
+	"use strict";
+	var v=document.getElementById('div_tittle');
+	v.style.cssText="width:720px";
+}
