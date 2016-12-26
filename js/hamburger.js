0fc1a1eb35e75278e08d1088a3b484e78cc8bbf0
WangJiaxiang96 <melody_mylady@outlook.com>
2016-12-26 20:09:32 +0800

    Update

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,6 +1,28 @@
 // JavaScript Document
+	
+function openPanel(){
+	"use strict";
+	var menuPanel=document.getElementById('menu_content');
+	menuPanel.style.cssText="left:0px;";
+	var menuBG=document.getElementById('menu_bg');
+	menuBG.style.cssText="display:block;";
+}
 
-function(){
-	
-	
+function collapsePanel(){
+	"use strict";
+	var menuPanel=document.getElementById("menu_content");
+	menuPanel.style.cssText="left:-200px;";
+	var menuBG=document.getElementById("menu_bg");
+	menuBG.style.cssText="display:none;";
+}
+
+function hamburgerMenu(){	
+    "use strict";
+	var leftValue=document.getElementById('menu_content').style.left;
+	if(leftValue==="0px"){
+		collapsePanel();
 	}
+	else{
+		openPanel();
+	}	
+}
