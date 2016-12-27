d69eef1b496f7b02ffd0c931cb23667a0ba9d640
WangJiaxiang96 <melody_mylady@outlook.com>
2016-12-28 00:26:15 +0800

    Update

--- js/js_Common(PC&Mobile).js
+++ js/js_Common(PC&Mobile).js
@@ -5,8 +5,7 @@ $(function(){
 $('.targetC').click(function(){
 	    "use strict";
 	    $('html,body').animate({
-			scrollTop:$('#div_codeMonkey').offset().top
-			},600);
+			scrollTop:$('#div_codeMonkey').offset().top},600);
 	}
 );});
 
@@ -15,15 +14,19 @@ $(function(){
 $('.targetM').click(function(){
 	    "use strict";
 	    $('html,body').animate({
-			scrollTop:$('#div_musicProducer').offset().top
-			},600);
+			scrollTop:$('#div_musicProducer').offset().top},600);
 });});
 
 //滑动到顶部
 $(function(){ 
 $('#a_main').click(function(){
 	    "use strict";
-	    $('html,body').animate({
-			scrollTop:0
-			},600);
+		var t=$(document).scrollTop() - $('#listMusic').offset().top;
+		if(t>0){
+	    $('html,body').animate({scrollTop:0},600);
+		}
+		else {
+			window.open("http://wangjiaxiang.net");
+		}
+			
 });});
