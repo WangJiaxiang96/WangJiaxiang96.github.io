ab08b0a538871193cbb558272b20500914d4bba3
WangJiaxiang96 <melody_mylady@outlook.com>
2016-12-28 01:54:46 +0800

    functions finished

--- js/js_Common(PC&Mobile).js
+++ js/js_Common(PC&Mobile).js
@@ -1,27 +1,5 @@
 // JavaScript Document
 
-//滑动到Code部分
-$(function(){ 
-$('.targetC').click(function(){
-	    "use strict";
-	    $('html,body').animate({
-			scrollTop:$('#div_codeMonkey').offset().top},600);
-		var s=$(this).attr('id');
-		if(s==='ul1_1'){ sliding2L('border1');}
-		if(s==='ul1_2'){ sliding2L('border1');}
-	}
-);});
-
-//滑动到Music部分
-$(function(){ 
-$('.targetM').click(function(){
-	    "use strict";
-	    $('html,body').animate({
-			scrollTop:$('#div_musicProducer').offset().top},600);
-		var s=$(this).attr('id');
-		if(s==='ul2_1'){ sliding2L('border2');}
-		if(s==='ul2_2'){ sliding2L('border2');}
-});});
 
 //滑动到顶部
 $(function(){ 
