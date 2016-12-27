620631df9e0bc50e291f02f8ddd8c78fffaaccc7
WangJiaxiang96 <melody_mylady@outlook.com>
2016-12-27 13:15:41 +0800

    Update

---
+++ js/js_PC.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+function widthIncreased(){
+	"use strict";
+	var v=document.getElementById('div_tittle');
+	v.style.cssText="width:980px;";
+}
+function widthDecreased(){
+	"use strict";
+	var v=document.getElementById('div_tittle');
+	v.style.cssText="width:710px";
+}
+
+function pageLoaded(){
+	"use strict";
+	var h=window.screen.height;
+	var th1=document.getElementById('div_TH1');
+	var miracle=document.getElementsByClassName('div_miracle');
+	th1.style.cssText="height:"+h+"px;";
+	miracle.style.cssText="height:"+h+"px;";
+}
