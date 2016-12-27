58f1feb11e12048653937d26559b3eae2af0f7db
WangJiaxiang96 <melody_mylady@outlook.com>
2016-12-27 15:35:10 +0800

    Update

--- js/js_PC.js
+++ js/js_PC.js
@@ -5,6 +5,7 @@ function widthIncreased(){
 	var v=document.getElementById('div_tittle');
 	v.style.cssText="width:955px;";
 }
+
 function widthDecreased(){
 	"use strict";
 	var v=document.getElementById('div_tittle');
@@ -13,11 +14,14 @@ function widthDecreased(){
 
 function pageLoaded(){
 	"use strict";
-	var h=window.screen.availHeight;
+	var w=window.screen.width;
+	if(w>=960) {			
+	var h=window.screen.height;	
 	var th1=document.getElementById('div_TH1');
 	var codeMonkey=document.getElementById('div_codeMonkey');
 	var musicProducer=document.getElementById('div_musicProducer');
 	th1.style.cssText="height:"+h+"px;";
 	musicProducer.style.cssText="height:"+(h*0.8)+"px;";
 	codeMonkey.style.cssText="height:"+(h*0.8)+"px;";
+	}
 }
